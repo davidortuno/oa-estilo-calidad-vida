6567d25e95f9ce6086c5c3c847fedb4c38ce3c09
David Torres Ortuño <holadavidortuno@gmail.com>
2026-05-28 15:59:57 -0600

    Se integra plugin quiz.js y actividad de prueba

---
+++ js/quiz-data.js
@@ -0,0 +1,26 @@
+$("#quiz1").dlxQuiz({
+  quizData: {
+    questions: [
+      {
+        q: "¿Por qué es importante cuidar la salud durante la adolescencia? ",
+        a: "Porque en la adolescencia se forman hábitos que pueden influir en la vida futura.",
+        options: [
+          "Porque en esta etapa no ocurren muchos cambios.",
+          "Porque en la adolescencia se forman hábitos que pueden influir en la vida futura.",
+          "Porque los adolescentes ya tienen completamente definida su identidad.",
+          "Porque es la etapa con menos riesgos para la salud",
+        ],
+      },
+      {
+        q: "¿Cuáles son algunos de los problemas de salud más comunes que enfrentan los adolescentes según el texto?",
+        a: "Accidentes de tránsito, estrés, ansiedad, depresión y mala alimentación.",
+        options: [
+          "Problemas relacionados con enfermedades respiratorias.",
+          "Robo, violencia escolar y problemas económicos.",
+          "Accidentes de tránsito, estrés, ansiedad, depresión y mala alimentación.",
+          "Exclusivamente consumo de alcohol.",
+        ],
+      },
+    ],
+  },
+});
